a14b69f2bd48f65069e3738a6d75956b7fc856b0
Arkadiusz Lis <alis@efigence.com>
2020-01-08 16:29:17 +0100

    setState

---
+++ burger-schwartz/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Manu", age: "28" },
+      { name: "Max", age: "24" },
+      { name: "Sam", age: "20" }
+    ]
+  };
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: "Manuel", age: "28" },
+        { name: "Max", age: "24" },
+        { name: "Sam", age: "99" }
+      ]
+    });
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to React Shwartz</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          Some additional information
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
