ce1b8d31588595c49fae2a5d4c85437b8dc1400b
Arkadiusz Lis <alis@efigence.com>
2020-01-09 16:13:17 +0100

    flexibleLists

--- burger-schwartz/src/App.js
+++ burger-schwartz/src/App.js
@@ -5,42 +5,63 @@ import Person from "./Person/Person";
 class App extends Component {
   state = {
     persons: [
-      { name: "Manu", age: "28" },
-      { name: "Max", age: "24" },
-      { name: "Sam", age: "20" }
-    ]
+      { id: "1", name: "Manu", age: "28" },
+      { id: "2", name: "Max", age: "24" },
+      { id: "3", name: "Sam", age: "20" }
+    ],
+    showPersons: false
   };
-  switchNameHandler = name => {
+
+  deletePersonHandler = (index) => {
+    let newPersons = this.state.persons.filter((el, i)=> i !== index)
+    this.setState ({
+      persons: newPersons
+    })
+  }
+  togglePersonsHandlers = () => {
+    let show = this.state.showPersons;
     this.setState({
-      persons: [
-        { name: name, age: "28" },
-        { name: "Max", age: "24" },
-        { name: "Sam", age: "99" }
-      ]
+      showPersons: !show
     });
   };
+  changeInputHandler = (event, idPerson) => {
+   let personId = this.state.persons.findIndex((el)=> el.id === idPerson);
+
+    const person = { ...this.state.persons[personId] };
+    person.name = event.target.value
+    const persons = [...this.state.persons];
+    persons[personId] = person;
+
+    this.setState({
+      persons: persons
+    })
+  }
   render() {
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map(({id, name, age }, index) => {
+            return (
+              <Person
+                key={id}
+                name={name}
+                age={age}
+                click={() => this.deletePersonHandler(index)}
+                val={name}
+                changed={event => this.changeInputHandler(event, id)}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Welcome to React Shwartz</h1>
-        <button onClick={this.switchNameHandler.bind(this, "Maksikon")}>
-          Switch Name
-        </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={() => this.switchNameHandler("Szczałkom")}
-        >
-          Some additional information
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+        <button onClick={this.togglePersonsHandlers}>Togle Name</button>
+        {persons}
       </div>
     );
   }
--- burger-schwartz/src/Person/Person.js
+++ burger-schwartz/src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 const person = props => {
-  const { name, age, click, children } = props;
+  const { name, age, click, children, val, changed } = props;
   return (
     <div>
+      <input type="text" value={val} onChange={changed} />
       <p onClick={click}>
         I'm a {name} and I'm {age} years old!{" "}
       </p>
